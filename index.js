96ebfcc4da9e07cd63f5a5698abd606c28e5b2c7
chandrashekar <chandujobsapplications@gmail.com>
2023-04-22 19:16:53 +0530

    error msg added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app=express();
 app.use('/',require("./routes"));
 app.listen(port,(err)=>{
     if(err){
-        console.log(err);
+        console.log("error",err);
         return;
     }
     console.log("server started at port ",port);
