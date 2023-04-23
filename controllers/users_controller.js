15c761960b057b0ece640e78edd199525c00b0e8
chandrashekar <chandujobsapplications@gmail.com>
2023-04-23 09:21:42 +0530

    created sign in and sign up forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = function(req, res){
         title: 'User Profile'
     })
 }
+
+module.exports.signUp=(req,res)=>{
+    return res.render("user_sign_up",{
+        title:"codeial | sign up"
+    })
+}
+
+//render sign in page
+module.exports.signIn=(req,res)=>{
+    return res.render("user_sign_in",{
+        title:"codeial | sign In"
+    })
+}
+
+module.exports.create=(req,res)=>{
+    //todo later
+}
+module.exports.createSession=(req,res)=>{
+    //todo later
+}
