42339a553b2ab8bfbd6c9be2b57af31161f6eb6d
chandrashekar <chandujobsapplications@gmail.com>
2023-04-23 16:50:06 +0530

    added ascync function to findOne()

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
+const User=require("../models/user")
+
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: 'User Profile'
@@ -17,9 +20,34 @@ module.exports.signIn=(req,res)=>{
     })
 }
 
-module.exports.create=(req,res)=>{
-    //todo later
-}
+// module.exports.create=async (req,res)=>{
+//    if(req.body.password!=req.body.confirm_password){
+//     return res.redirect("back");
+//    }
+//    let user=await User.findOne({email:req.body.email});
+//     
+//     if(!user){
+//         await User.create(req.body);
+// return res.redirect("/users/sign-in")
+
+//     }
+//         return res.redirect("back");
+//}
+
+
 module.exports.createSession=(req,res)=>{
     //todo later
 }
+
+
+module.exports.create = async function (req, res) {
+    if (req.body.password != req.body.confirm_password) {
+      return res.redirect("back");
+    }
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      await User.create(req.body);
+      return res.redirect("/users/sign-in");
+    }
+    return res.redirect("back");
+  };
